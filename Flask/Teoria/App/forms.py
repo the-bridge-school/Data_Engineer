ec6225be72c00fc31fa6a6768496380f03431496
Juan_Maniglia <90753457+JuanManiglia@users.noreply.github.com>
2023-06-21 00:30:56 +0200

    cuarto paso

--- Flask/Teoria/App/forms.py
+++ Flask/Teoria/App/forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm 
 from wtforms import StringField, SubmitField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length
 
