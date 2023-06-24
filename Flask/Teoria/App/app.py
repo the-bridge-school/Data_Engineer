9f17ab51ea41777ff05df658939f49feb11bfdaa
Juan_Maniglia <90753457+JuanManiglia@users.noreply.github.com>
2023-06-24 11:01:53 +0200

    sube correccion de notebooks

---
+++ Flask/Teoria/App/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object("config")
+app.config.from_pyfile("config.py")
+
+
+@app.route('/')
+def index():
+    return "<h1>Hello World !</h1>"
+
+# app.add_url_rule("/","index",index)
+
+@app.route("/user/<name>")
+def user(name: str):
+    return "<h1>Hello , {}!</h1>".format(name)
+
+@app.route("/user1/<name>")
+def user1(name):
+    return "<h1>Bye, {}!</h1>".format(name)
---
+++ Flask/Teoria/App/config.py
@@ -0,0 +1,4 @@
+DEBUG = True
+BCRYPT_LOG_ROUNDS = 12 # configuración para la extensión Flask-Bcrypt
+MAIL_FROM_EMAIL = "micorreo@gmail.com"
+FLASK_APP = "app.py"
